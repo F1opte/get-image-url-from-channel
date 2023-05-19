8610c327c5d1c42e4cdd03b7f84f805c22b9f874
F1opte <112293794+F1opte@users.noreply.github.com>
2023-05-19 21:09:59 +0700

    Update d.py

--- d.py
+++ d.py
@@ -1,25 +1,24 @@
 import tls_client, time, colorama
 
-session = tls_client.Session(client_identifier="chrome110",h2_settings={"HEADER_TABLE_SIZE": 65536,"MAX_CONCURRENT_STREAMS": 1000,"INITIAL_WINDOW_SIZE": 6291456,"MAX_HEADER_LIST_SIZE": 262144},random_tls_extension_order=True)
-token = ""
-webhook = ""
-a = []
-channel = input("Channel id : ")
+req = tls_client.Session(client_identifier="chrome110",h2_settings={"HEADER_TABLE_SIZE": 65536,"MAX_CONCURRENT_STREAMS": 1000,"INITIAL_WINDOW_SIZE": 6291456,"MAX_HEADER_LIST_SIZE": 262144},random_tls_extension_order=True)
+a = "token"
+b = "webhook url"
+c = []
+d = input("Channel id : ")
 
 def get_url():
-    r = session.get(f"https://discord.com/api/v9/channels/{channel}/messages", headers={"authorization": token})
+    r = req.get(f"https://discord.com/api/v9/channels/{d}/messages", headers={"authorization": a})
     if r.status_code == 200:
-        messages = r.json()
-        for message in messages:
-            if "attachments" in message:
-                for attachment in message['attachments']:
-                    if "url" in attachment:
-                        print(f"{colorama.Fore.LIGHTGREEN_EX}GET {colorama.Fore.LIGHTYELLOW_EX}- {colorama.Fore.LIGHTWHITE_EX}{attachment['url']}")
-                        a.append(attachment['url'])
+        for i in r.json():
+            if "attachments" in i:
+                for _ in i['attachments']:
+                    if "url" in _:
+                        print(f"{colorama.Fore.LIGHTGREEN_EX}GET {colorama.Fore.LIGHTYELLOW_EX}- {colorama.Fore.LIGHTWHITE_EX}{_['url']}")
+                        c.append(_['url'])
 
 get_url()
-for _ in a:
-    r = session.post(webhook, json={"content":_})
+for _ in c:
+    r = session.post(b, json={"content":_})
     if r.status_code == 204:
         print(f"{colorama.Fore.LIGHTGREEN_EX}SENT {colorama.Fore.LIGHTYELLOW_EX}- {colorama.Fore.LIGHTWHITE_EX}{_}")
     elif "retry_after" in r.text:
